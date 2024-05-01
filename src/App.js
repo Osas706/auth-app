e142ef54f4b5c283c936c145f6a2752a8dbbd771
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-05-01 12:24:08 +0100

    Username ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import './App.css';
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound.jsx';
 
-function App() {
+//root routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/password',
+    element: <Password />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/recovery',
+    element: <Recovery />
+  },
+  {
+    path: '/reset',
+    element: <Reset />
+  },
+  {
+    path: '/*',
+    element: <PageNotFound />
+  },
+]);
+
+
+const App = () => {
   return (
-    <div className="App">
-     hi
-    </div>
-  );
+    <main>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App
